6a63be6f0f3bd4584c7e965b6d22b5041a2049e3
n3dhir <nadhir.halbouni5@gmail.com>
2026-02-05 15:26:02 +0100

    feat: Add scraped geographical data and integrate it into the Tunisia map component.

--- src/components/TunisiaMap.jsx
+++ src/components/TunisiaMap.jsx
@@ -452,8 +452,8 @@ function TunisiaMap({ currentLevel, selectedRegion, navigationPath, governorates
             zoomControl={true}
         >
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
             <BoundsFitter bounds={bounds || tunisiaBounds} />
