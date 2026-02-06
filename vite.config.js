eafbdf64ad6f105f9dd6d51216ce7f7d6d735ffd
azisauce <turkiazizu012@gmail.com>
2026-02-06 19:26:35 +0100

    Fix .gitignore to correctly ignore the dist directory and update vite.config.js for build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/map/',
     plugins: [react()],
+    build: {
+        outDir: '../administrator-server/public/map',
+        emptyOutDir: true,
+    }
 })
